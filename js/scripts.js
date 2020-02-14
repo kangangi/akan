3fbc424a331e9a8a7ef1e0cc0868484f2a6d87e9
kangangi <wanjikukangangi@gmail.com>
2020-02-14 08:34:10 +0300

    Add divs for formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
-
-
 function readData(){
 var dd = document.getElementById("dd").value;
 var mm = document.getElementById("mm").value;
-var cc = document.getElementById("cc").value;
 var yy = document.getElementById("yy").value;
-
 
 if (document.getElementById("male").checked){
   var male = true;
@@ -13,22 +9,11 @@ if (document.getElementById("male").checked){
 if (document.getElementByid("female").checked){
   var female = true;
 }
-
+return dd;
+return mm;
+return yy;
 }
 
-
-//var males = ["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"];
-//var females = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-
-//var dayOfTheWeek = function(cc,yy,mm,dd)
-//var day  =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )% 7;
-  //return day;
-//}
-
-//var akanName = function(day,male){
-//if (male === true){
-    //alert("Your Akan name is " + males[day] + "!");
-  //}else {
-  //  alert("Your Akan name is " + females[day] + "!");
-//}
+var birthday = new Date(mm + '/' + dd + '/' + yy);
+var day1 = birthday.getDay();
+alert(day1);
