a2bd61682be989e3fde6b34ed62d23b9050d343c
kangangi <wanjikukangangi@gmail.com>
2020-02-14 14:36:05 +0300

    Add comments to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -13,20 +13,22 @@ if (document.getElementById("male").checked){
 }else{
   var female = true;
 }
-//Allocate akan names
+//calculation of day of the week
 var males = ["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"];
 var females = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var birthday = new Date(mm + '/' + dd + '/' + yy);
 var day1 = birthday.getDay();
 
-//Validitation
+//Validitation and allocation of names
   if (dd <= 0 || dd >= 31){
   document.getElementById("display").innerText = "Sorry your date is invalid, please enter a valid date!"
   } else if(mm <= 0 || mm >= 13){
   document.getElementById("display").innerText = "Sorry your month is invalid, please enter a valid month!"
   }else if (male === true){
   document.getElementById("display").innerText = "Your Akan name is " + males[day1] + "!"
-  }else{
+  }else if (female === true){
     document.getElementById("display").innerText = "Your Akan name is " + females[day1] + "!"
+  }else{
+    document.getElementById("display").innerText = "Please select Gender "
   }
 }
