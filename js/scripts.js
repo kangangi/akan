c34d5d6f159f359c2e7dd10ab78941392fdecfed
kangangi <wanjikukangangi@gmail.com>
2020-02-14 11:51:10 +0300

    add formular for calculating day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+document.getElementById("submit").addEventListener("click",function(event){
+  event.preventDefault()
+})
+
+//get input from HTML
 function readData(){
 var dd = document.getElementById("dd").value;
 var mm = document.getElementById("mm").value;
@@ -5,15 +10,23 @@ var yy = document.getElementById("yy").value;
 
 if (document.getElementById("male").checked){
   var male = true;
-}
-if (document.getElementByid("female").checked){
+} else{
   var female = true;
 }
-return dd;
-return mm;
-return yy;
-}
-
+//Allocate akan names
+var males = ["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"];
+var females = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var birthday = new Date(mm + '/' + dd + '/' + yy);
 var day1 = birthday.getDay();
-alert(day1);
+
+//Validity
+if (dd <= 0 || dd >= 31){
+  document.getElementById("display").innerText = "Sorry your date is invalid, please enter a valid date!"
+} else if(mm <= 0 || mm >= 13){
+  document.getElementById("display").innerText = "Sorry your month is invalid, please enter a valid month!"
+} else if (male === true){
+  document.getElementById("display").innerText = "Your Akan name is " + males[day1] + "!"
+} else{
+  document.getElementById("display").innerText = "Your Akan name is " + females[day1] + "!"
+}
+}
